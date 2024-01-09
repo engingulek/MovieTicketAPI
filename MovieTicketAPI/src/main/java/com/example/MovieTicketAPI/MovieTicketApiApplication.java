dd83c71e85279f6d64930188971c61ac11c90739
Engin Gülek <engingulek0@gmail.com>
2024-01-09 19:02:01 +0300

    Created Project

---
+++ MovieTicketAPI/src/main/java/com/example/MovieTicketAPI/MovieTicketApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieTicketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketApiApplication.class, args);
+	}
+
+}
---
+++ MovieTicketAPI/src/test/java/com/example/MovieTicketAPI/MovieTicketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieTicketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
